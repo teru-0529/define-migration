f48fb6f04e46542b992ad2aa285d201aa4d9bd81
Teruaki.Sato <andrea.pirlo.0529@gmail.com>
2024-03-03 22:11:47 +0900

    host名解決

--- models/migration/migration.go
+++ models/migration/migration.go
@@ -2,10 +2,11 @@ package migration
 
 import (
 	"fmt"
-	// "github.com/golang-migrate/migrate/v4"
-	// _ "github.com/golang-migrate/migrate/v4/database/postgres"
-	// _ "github.com/golang-migrate/migrate/v4/source/file"
-	// _ "github.com/golang-migrate/migrate/v4/source/github"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/golang-migrate/migrate/v4/source/github"
 )
 
 type MigrationIO struct {
@@ -21,21 +22,35 @@ func (io *MigrationIO) Show() {
 }
 
 // INFO: UP
-func MigrateUp(migrationIo MigrationIO) error {
-	fmt.Println("-----", migrationIo.Schema, "migrate-up", "-----")
-	migrationIo.Show()
-	fmt.Println(migrationIo.Schema, "up")
+func MigrateUp(io MigrationIO) error {
+	fmt.Println("-----", io.Schema, "migrate-up", "-----")
+	io.Show()
+	err := getMigration(io)
+	if err != nil {
+		return err
+	}
+	fmt.Println(io.Schema, "up")
 	return nil
 }
 
 // INFO: DOWN
-func MigrateDown(migrationIo MigrationIO) error {
-	fmt.Println("-----", migrationIo.Schema, "migrate-down", "-----")
-	migrationIo.Show()
-	fmt.Println(migrationIo.Schema, "down")
+func MigrateDown(io MigrationIO) error {
+	fmt.Println("-----", io.Schema, "migrate-down", "-----")
+	io.Show()
+	err := getMigration(io)
+	if err != nil {
+		return err
+	}
+	fmt.Println(io.Schema, "down")
 	return nil
 }
 
-// func getMigration(schemaName string) {
-// 	source:=
-// }
+func getMigration(io MigrationIO) error {
+	m, err := migrate.New(io.Source, io.Database)
+	if err != nil {
+		return err
+	}
+	version, dirty, err := m.Version()
+	fmt.Println(version, dirty, err)
+	return nil
+}
